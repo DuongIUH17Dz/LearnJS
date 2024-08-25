6015409ab688c75056b6c00fe2af4003d659bcb2
H82M19\Student <Student>
2024-08-25 10:45:03 +0700

    Ex3 funcion winner

--- Ex3.js
+++ Ex3.js
@@ -3,4 +3,12 @@ function calculateAverage(scores) {
     const total = scores.reduce((acc, score) => acc + score, 0);
     return total / scores.length;
   }
+  // Function to determine the winner
+function determineWinner(dolphinsScores, koalasScores) {
+    const dolphinsAverage = calculateAverage(dolphinsScores);
+    const koalasAverage = calculateAverage(koalasScores);
+  
+    console.log(`Dolphins' average score: ${dolphinsAverage.toFixed(2)}`);
+    console.log(`Koalas' average score: ${koalasAverage.toFixed(2)}`);
+}
   
