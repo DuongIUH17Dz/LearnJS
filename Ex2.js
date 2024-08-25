ce99e61f26e3376f88882d1bc402c8ae7a3a67b1
H82M19\Student <Student>
2024-08-25 10:38:54 +0700

    Test data 1

---
+++ Ex2.js
@@ -0,0 +1,10 @@
+
+// Test data 1
+const markWeight1 = 78; // kg
+const markHeight1 = 1.69; // m
+const johnWeight1 = 92; // kg
+const johnHeight1 = 1.95; // m
+
+// Calculate BMI for Mark and John (Data 1)
+const markBMI1 = markWeight1 / (markHeight1 * markHeight1);
+const johnBMI1 = johnWeight1 / (johnHeight1 * johnHeight1);
